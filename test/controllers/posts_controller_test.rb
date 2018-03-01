12a6a8c7def768d2b1c6892cb7802d527dca04f5
Mary Franks <maryfranks@gmail.com>
2018-03-01 11:12:45 -0500

    write index and root tests for posts controller

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -4,10 +4,14 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
 
   test "index" do
     skip
+    get posts_url
+    assert_response :success
   end
-
+  
   test "root" do
     skip
+    get root_url
+    assert_response :success
   end
 
 end
