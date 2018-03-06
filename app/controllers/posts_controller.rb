0c80a42905fb7d0a9d5b1100492ee8396f51f1ee
Mary Franks <maryfranks@gmail.com>
2018-03-06 14:37:16 -0500

    link form to database - save new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,23 @@ class PostsController < ApplicationController
 
   def index
     @post = Post.new
+    @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    @post.text = params[:post][:text]
+    if @post.save
+      redirect_to posts_path
+      flash[:notice] = "Post Created"
+    else
+      flash[:error] = "Oops, try again - Posts must have text!"
+    end
+
+  end
 
 end
