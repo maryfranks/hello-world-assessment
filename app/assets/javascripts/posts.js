d935b61dc0708ea2aa4250d1efd91c995d34cac6
Mary Franks <maryfranks@gmail.com>
2018-03-06 16:58:07 -0500

    submit post via ajax

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,3 +1,27 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+document.addEventListener("DOMContentLoaded", function() {
+  var postDoneButton = document.querySelector('#submit-post');
+  var allPosts = document.querySelector('#all-posts');
+  var post = document.querySelector('#post_text')
+
+  postDoneButton.addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log("Submitting post");
+    $.ajax({
+      url: '/posts',
+      method: 'post',
+      data: $(post).serialize(),
+      // dataType: 'json',
+    }).done(function(data) {
+      console.log("done");
+      var newPost = document.createElement('p');
+      newPost.innerText = data;
+      allPosts.append(newPost);
+      postDoneButton.reset();
+      // message_button.disabled = false;
+      console.log("all finished");
+    });
+  });
+});
