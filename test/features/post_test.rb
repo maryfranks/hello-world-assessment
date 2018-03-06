58099d47161349c4adfc70042176fa424692c8e1
Mary Franks <maryfranks@gmail.com>
2018-03-06 11:24:29 -0500

    test to submit new post via form

--- test/features/post_test.rb
+++ test/features/post_test.rb
@@ -6,4 +6,29 @@ feature "Post" do
   #   page.must_have_content "Hello World"
   #   page.wont_have_content "Goodbye All!"
   # end
+  test "post submission form exists" do
+    skip
+    visit "posts"
+    assert page.has_field? "text", type: "text"
+  end
+
+  test "submit new post via form" do
+    skip
+    visit "posts"
+    fill_in "text", :with => "Hello World"
+    click_on "Submit"
+    # page should reload when form is submitted
+    assert_equal "/pages", current_path
+    assert_selector "p", text: "Hello World"
+    assert_equal "Post Created", flash[:notice]
+  end
+
+  test "displays flash when post with no text does not save" do
+    skip
+    visit "posts"
+    click_on "Submit"
+    assert_equal "/pages", current_path
+    assert_equal "Oops, try again - Posts must have text!", flash[:error]
+  end
+
 end
