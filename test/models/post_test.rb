a1cb6e23c442d22d33142f0ba430e1d7d1a1c028
Mary Franks <maryfranks@gmail.com>
2018-03-06 10:01:32 -0500

    test to create post

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -6,4 +6,19 @@ describe Post do
   it "must be valid" do
     value(post).must_be :valid?
   end
+
+  test "can create post" do
+    skip
+    post = Post.new
+    post.text = "hello"
+    assert post.save
+  end
+
+  test "post needs text" do
+    skip
+    post = Post.new
+    post.save
+    refute post.save
+  end
+
 end
