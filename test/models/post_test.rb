41e8baab68bae7cc031fde98d20195ce8e4c0c84
Mary Franks <maryfranks@gmail.com>
2018-03-07 14:24:12 -0500

    add tests for user

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,9 +4,11 @@ describe Post do
   # let(:post) { Post.new }
 
   test "can create post" do
-    # skip
+    skip
+    user = User.create(username: "pooh", password: "1234", password_confirmation: "1234")
     post = Post.new
     post.text = "hello"
+    post.user_id = user
     assert post.save
   end
 
@@ -14,6 +16,12 @@ describe Post do
     # skip
     post = Post.new
     post.save
+    refute post.save
+  end
+
+  test "post needs user" do
+    skip
+    post = Post.new(text: "goodbye")
     refute post.save
   end
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,4 +6,30 @@ describe User do
   it "must be valid" do
     value(user).must_be :valid?
   end
+
+  test "can create user" do
+    skip
+    user = User.new(username: "superuser", password: "1234", password_confirmation: "1234")
+    assert user.save
+  end
+
+  test "user must have username" do
+    skip
+    user = User.new(password: "1234", password_confirmation: "1234")
+    refute user.save
+  end
+
+  test "username must be unique" do
+    skip
+    user1 = User.create(username: "jsmith", password: "1234", password_confirmation: "1234")
+    user2 = User.new(username: "jsmith", password: "5678", password_confirmation: "5678")
+    refute user.save
+  end
+
+  test "user must have password and password confirmation" do
+    skip
+    user = User.new(username: "awesomeperson", password: "1234")
+    refute user.save
+  end
+
 end
