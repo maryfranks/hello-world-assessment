208093da6b7ca5c38cd7d6d94cfe9b2aa5398c8f
Mary Franks <maryfranks@gmail.com>
2018-03-08 17:05:29 -0500

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+Post.delete_all
+
+ruby_posts = ["red", "green", "blue", "purple", "yellow"]
+jason_posts = ["snake", "frog", "cat", "bird"]
+
+ruby = User.create(username: "ruby", password: "1234", password_confirmation: "1234")
+jason = User.create(username: "jason", password: "1234", password_confirmation: "1234")
+
+ruby_posts.each do |post|
+  post = Post.create(user_id: ruby.id, text: post)
+end
+
+jason_posts.each do |post|
+  post = Post.create(user_id: jason.id, text: post)
+end
