38e9882c623c789fc263e9d78ce7e553fcd7515c
Mary Franks <maryfranks@gmail.com>
2018-03-08 11:57:20 -0500

    fix typo

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -4,7 +4,7 @@ describe SessionsController do
 
   test "user can sign out" do
     # skip
-    delete session_url(:martine)
+    delete session_url(:joe)
     assert_redirected_to root_url
   end
 
