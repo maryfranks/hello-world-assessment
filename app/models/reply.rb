48bc7335a3abdd6fa8b1af762612c9850f8da710
Mary Franks <maryfranks@gmail.com>
2018-03-10 14:59:38 -0500

    add data validation, reply must have text

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,6 +1,8 @@
 class Reply < ApplicationRecord
 
   belongs_to :user
-  belongs_to :post 
+  belongs_to :post
+
+  validates :reply, presence: true 
 
 end
--- test/models/reply_test.rb
+++ test/models/reply_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 describe Reply do
 
   test "reply must have text" do
-    skip
+    # skip
     reply = Reply.new(user_id: User.first.id, post_id: Post.first.id)
     refute reply.save
   end
