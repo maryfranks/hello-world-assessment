5894f32e6bdeeffbcdd33d0387f23407edff7d4c
Mary Franks <maryfranks@gmail.com>
2018-03-10 08:47:00 -0500

    create replies table

--- db/migrate/20180310133201_create_replies.rb
+++ db/migrate/20180310133201_create_replies.rb
@@ -1,6 +1,9 @@
 class CreateReplies < ActiveRecord::Migration[5.1]
   def change
     create_table :replies do |t|
+      t.text      :reply
+      t.integer   :user_id
+      t.integer   :post_id
 
       t.timestamps
     end
