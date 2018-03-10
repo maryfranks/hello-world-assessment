671f547ee24e47090fad62bbfe377c504e88c2ac
Mary Franks <maryfranks@gmail.com>
2018-03-10 14:41:09 -0500

    add tests for replies

--- test/controllers/replies_controller_test.rb
+++ test/controllers/replies_controller_test.rb
@@ -1,7 +1,42 @@
 require "test_helper"
 
-describe RepliesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+class PostsControllerTest < ActionDispatch::IntegrationTest
+
+  describe RepliesController do
+
+    include SignInHelper
+
+    setup do
+      @user = users(:joe)
+      sign_in_as users(:joe)
+      @joes_post = posts(:hello)
+      @sues_post = posts(:dogs)
+    end
+
+    test "can reply to own post" do
+      skip
+      assert_difference('Reply.count') do
+        post post_replies_url(@joes_post.id), params: { reply: { reply: "I like your post" } }
+      end
+      assert_equal "reply posted", flash[:notice]
+    end
+
+    test "can reply to other post" do
+      skip
+      assert_difference('Reply.count') do
+        post post_replies_url(@sues_post.id), params: { reply: { reply: "I like your post" } }
+      end
+      assert_equal "reply posted", flash[:notice]
+    end
+
+    test "post save fail" do
+      skip
+      assert_no_difference('Reply.count') do
+        post post_replies_url(@joes_post.id), params: { reply: { reply: nil } }
+      end
+      assert_equal "reply must have text", flash[:error]
+    end
+
+  end
+
 end
