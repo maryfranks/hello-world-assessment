7a73bd1bfe8ef940a5a5c8c3d8687a53c0e541f8
Mary Franks <maryfranks@gmail.com>
2018-03-13 17:19:52 -0400

    add latitude, longitude, and weather to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180313143824) do
+ActiveRecord::Schema.define(version: 20180313211800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,9 @@ ActiveRecord::Schema.define(version: 20180313143824) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.string "city"
+    t.string "long"
+    t.string "lat"
+    t.string "weather"
   end
 
   create_table "replies", force: :cascade do |t|
